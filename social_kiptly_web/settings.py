e8498199a8f0ef378f0e2577e9f9aae57bbfeb4b
reymanuel010899 <reymanuelferrera78@gmail.com>
2023-04-15 15:54:55 -0400

    v15

--- social_kiptly_web/settings.py
+++ social_kiptly_web/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import venv
 # Don't forget to import dj-database-url at the beginning of the file
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
