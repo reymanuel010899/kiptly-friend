fb769694b5df55333b3c2f87c5dd2d60d0bbc57f
reymanuel010899 <reymanuelferrera78@gmail.com>
2023-04-16 11:50:22 -0400

    v36

--- home/forms.py
+++ home/forms.py
@@ -2,9 +2,15 @@ from django.forms import ModelForm
 from django import forms
 from users.models import User
 from django.forms import ClearableFileInput
+
 class  userupdateavatar(ModelForm):
-    avatar = forms.FileField(widget=ClearableFileInput(attrs={'Currently':False}))
+    avatar = forms.FileField(required=False)
     class Meta:
         model=User
         fields = ('avatar',)
        
+    
+    def __init__(self, *args, **kwars):
+        super(userupdateavatar, self).__init__(*args, **kwargs)
+        self.fields['avatar'].initial = None
+        
