b0af6d8606a8b324d117d8c789e68d24a7c92e28
reymanuel010899 <reymanuelferrera78@gmail.com>
2023-04-16 15:51:13 -0400

    v47

--- social_kiptly_web/settings.py
+++ social_kiptly_web/settings.py
@@ -132,18 +132,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
     ]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 if not DEBUG:
+
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = os.environ.get('EMAIL_HOST')
     EMAIL_PORT = os.environ.get('EMAIL_PORT')
--- social_kiptly_web/urls.py
+++ social_kiptly_web/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static, serve
+from django.conf.urls.static import static
 #from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
@@ -29,6 +29,3 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
-# urlpatterns += [
-#     re_path(r'˄media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT,})
-# ]
