fd058e740cb799841fcc5fd3f00002e175d82892
reymanuel010899 <reymanuelferrera78@gmail.com>
2023-04-16 17:53:02 -0400

    v50

--- social_kiptly_web/settings.py
+++ social_kiptly_web/settings.py
@@ -133,18 +133,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
+    BASE_DIR / "static",
+ 
 ]
-
-
 
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
