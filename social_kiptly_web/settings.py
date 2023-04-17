be2dc8160ab5a7cb0102d0a7dcba7a163671d08d
reymanuel010899 <reymanuelferrera78@gmail.com>
2023-04-16 22:57:43 -0400

    v57

--- social_kiptly_web/settings.py
+++ social_kiptly_web/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Don't forget to import dj-database-url at the beginning of the file
 import dj_database_url
-
+import json
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -154,8 +154,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# MEDIA_URL = "/media/"
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
--- social_kiptly_web/urls.py
+++ social_kiptly_web/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
-from .views import media
+from django.conf.urls.static import static, serve
+#from .views import media
 #from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
@@ -25,9 +25,16 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('inicio/', include('home.urls', namespace='inicio_app')),
     path('', include('users.urls', namespace='users_app')),
-    path('media/<str:path>/', media, name='media'),
+    #path('media/<str:path>/', media, name='media'),
 ]
-if settings.DEBUG:
 
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
+
+# if settings.DEBUG:
+
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
