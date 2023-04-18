c3ca62d5955f6f1f5fe3f2fbec1d05157ca687fd
reymanuel010899 <reymanuelferrera78@gmail.com>
2023-04-18 14:57:27 -0400

    raiways4

--- social_kiptly_web/settings.py
+++ social_kiptly_web/settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'social_kiptly_web.wsgi.application'
 DATABASES = {
      'default': {
     
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME':os.environ.get('RAILWAY_PG_URL'),
         'OPTIONS': {
             'sslmode': 'require',
